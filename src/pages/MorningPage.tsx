f00f0bd286a3fa9ab7d531839954f08118ca2434
dannyk20 <dannyk20@naver.com>
2024-07-17 04:56:17 +0900

    design: 디자인 및 코드 수정

--- src/pages/MorningPage.tsx
+++ src/pages/MorningPage.tsx
@@ -10,6 +10,10 @@ const MorningPage: React.FC = () => {
     const closeModal = () => {
         setIsModalOpen(false);
     };
+    const handleButtonClick = () => {
+        const audio = new Audio('src/assets/sounds/click.mp3');
+        audio.play();
+    };
 
     useEffect(() => {
         if (inputValue.trim() !== '') {
@@ -20,7 +24,7 @@ const MorningPage: React.FC = () => {
     }, [inputValue]);
 
     return (
-        <div className="flex items-center justify-center w-screen h-screen bg-cover bg-[url('src/assets/images/background/office_m.png')]">
+        <div className="flex flex-col justify-between w-screen h-screen bg-cover bg-[url('src/assets/images/background/office_m.png')]">
             {isModalOpen ? (
                 <div className="modal-overlay">
                     <div className="modal-container animate-modal">
@@ -28,25 +32,44 @@ const MorningPage: React.FC = () => {
                     </div>
                 </div>
             ) : (
-                <div className="flex flex-col items-center self-end justify-center animate-fade-in w-[45rem] h-[61rem] bg-contain bg-no-repeat bg-[url('src/assets/images/standing/nice_m_long.png')]">
-                    <div className='translate-y-3/4'>
-                        <div className="flex items-center justify-center w-[86.25rem] h-[11.125rem] bg-contain bg-no-repeat bg-[url('src/assets/images/others/script_ui.png')]">
-                            <p className="ml-12 mt-4 text-black text-center font-dgm text-[2.562rem]">다들 좋은 아침입니다.</p>
-                        </div>
-                        <div className="flex -mt-1 justify-between items-center w-[86.25rem] h-[5.4375rem] bg-contain bg-no-repeat bg-[url('src/assets/images/others/input_ui.png')]">
-                            <input 
-                                type="text" 
-                                placeholder="답변을 입력하세요" 
-                                className='flex-grow ml-10 text-4xl text-black bg-transparent border-none outline-none font-dgm'
-                                value={inputValue} 
-                                onChange={(e) => setInputValue(e.target.value)} 
-                            />
-                            <button className='flex-none'>
-                                <img src={buttonImage} alt="button" className='w-12 h-12 mr-9'/>
-                            </button>
+                <>
+                    <div className='flex justify-end p-4'>
+                        <button
+                            type="button" 
+                            className='flex items-center justify-center font-dgm text-[2.2rem] text-white mt-7 mr-10 hover:text-[#FFE486]'
+                            onClick={handleButtonClick}>
+                            피드백 받기 ! 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="26" viewBox="0 0 26 31" fill="none" className='ml-5'>
+                                <path d="M5.03009e-06 2.50001V28.4C5.03009e-06 30.375 2.175 31.575 3.85 30.5L24.2 17.55C24.5563 17.3245 24.8497 
+                                17.0126 25.0531 16.6433C25.2565 16.2739 25.3631 15.8591 25.3631 15.4375C25.3631 15.0159 25.2565 14.6011 25.0531 
+                                14.2317C24.8497 13.8624 24.5563 13.5505 24.2 13.325L3.85 0.40001C3.47293 0.156121 3.03701 0.0185269 2.58825 0.00174343C2.13948 
+                                -0.0150401 1.69451 0.0896088 1.30026 0.304651C0.90602 0.519692 0.577125 0.837154 0.348275 1.22355C0.119424 1.60994 -0.000896585 2.05093 5.03009e-06 2.50001Z" 
+                                fill="currentColor"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="flex items-end justify-center">
+                        <div className="flex flex-col items-center justify-end animate-fade-in w-[45.44rem] h-[61.56rem] bg-cover bg-[url('src/assets/images/standing/nice_m_long.png')]">
+                            <div className='-translate-y-1/2'>
+                                <div className="flex items-center justify-center w-[86.25rem] h-[11.125rem] bg-contain bg-no-repeat bg-[url('src/assets/images/others/script_ui.png')]">
+                                    <p className="ml-12 mt-4 text-black text-center font-dgm text-[2.562rem]">다들 좋은 아침입니다.</p>
+                                </div>
+                                <div className="flex -mt-1 justify-between items-center w-[86.25rem] h-[5.4375rem] bg-contain bg-no-repeat bg-[url('src/assets/images/others/input_ui.png')]">
+                                    <input 
+                                        type="text" 
+                                        placeholder="답변을 입력하세요" 
+                                        className='flex-grow ml-10 text-4xl text-black bg-transparent border-none outline-none font-dgm'
+                                        value={inputValue} 
+                                        onChange={(e) => setInputValue(e.target.value)} 
+                                    />
+                                    <button className='flex-none'>
+                                        <img src={buttonImage} alt="button" className='w-12 h-12 mr-9'/>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </>
             )}
         </div>
     );
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import '../index.css'; 
+import React, { useState } from 'react';
+import '../index.css';
 
 const RegisterPage: React.FC = () => {
+  const [isIdChecked, setIsIdChecked] = useState(false);
+
+  const handleIdCheck = () => {
+    setIsIdChecked(true);
+    alert('사용할 수 있는 아이디입니다.');
+  };
+
+  const handleButtonClick = () => {
+    const audio = new Audio('src/assets/sounds/click.mp3');
+    audio.play();
+  };
+
   return (
     <div className="flex items-center justify-between w-screen h-screen bg-cover bg-main_h">
       <div className='flex self-end'>
@@ -12,38 +24,59 @@ const RegisterPage: React.FC = () => {
           <h1 className="mt-[3.25rem] mb-[0.3rem] text-6xl text-center font-nomal gradient-text font-dnf text-border-norm">
             회원가입
           </h1>
-          <div className='flex flex-col mb-10'>
+          <div className='flex flex-col mb-5'>
+            <label className="mb-3 text-black font-dgm text-[1.4375rem]" htmlFor="password">이름</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="2글자 이상"
+              className="text-xl rounded-[0.875rem] w-[30rem] h-14 pl-5 bg-[#F0F0F0] text-[#B2B2B2] font-dgm"
+            />
+          </div>
+          <div className='flex flex-col mb-5'>
             <label className="mb-3 text-black font-dgm text-[1.4375rem]" htmlFor="아이디">아이디</label>
             <div className='flex items-center w-full space-x-4'>
               <input
-                  id="아이디"
+                  id="id"
                   type="text"
-                  placeholder="  영문+숫자"
-                  className="text-xl rounded-[0.875rem] w-80 h-16 bg-[#F0F0F0] text-[#B2B2B2] font-dgm mb-3."
+                  placeholder="영문+숫자"
+                  className="text-xl rounded-[0.875rem] w-80 h-14 pl-5 bg-[#F0F0F0] text-[#B2B2B2] font-dgm mb-3."
               />
-              <button className="rounded-[0.875rem] w-36 h-16 ml-[1.35] font-dgm text-[1.25rem] bg-[#505050] text-white cursor-pointer">중복확인</button>
+              <button
+                type="button"
+                className="rounded-[0.875rem] w-36 h-14 ml-[1.35] font-dgm text-[1.25rem] bg-[#505050] hover:bg-[#222222] text-white cursor-pointer"
+                onClick={handleIdCheck}
+              >
+                중복확인
+              </button>
             </div>
           </div>
-          <div className='flex flex-col mb-9'>
+          <div className='flex flex-col mb-5'>
             <label className="mb-3 text-black font-dgm text-[1.4375rem]" htmlFor="password">비밀번호</label>
             <input
               id="password"
               type="password"
-              placeholder="  6자리 이상"
-              className="text-xl rounded-[0.875rem] w-[30rem] h-16 bg-[#F0F0F0] text-[#B2B2B2] font-dgm"
+              placeholder="6자리 이상"
+              className="text-xl rounded-[0.875rem] w-[30rem] h-14 pl-5 bg-[#F0F0F0] text-[#B2B2B2] font-dgm"
             />
           </div>
-          <div className='flex flex-col mb-12'>
+          <div className='flex flex-col mb-10'>
             <label className="mb-3 text-black font-dgm text-[1.4375rem]" htmlFor="password">비밀번호 확인</label>
             <input
               id="password"
               type="password"
-              placeholder="  6자리 이상"
-              className="text-xl rounded-[0.875rem] w-[30rem] h-16 bg-[#F0F0F0] text-[#B2B2B2] font-dgm"
+              placeholder="6자리 이상"
+              className="text-xl rounded-[0.875rem] w-[30rem] h-14 pl-5 bg-[#F0F0F0] text-[#B2B2B2] font-dgm"
             />
           </div>
           <div className='flex flex-col items-center mb-10'>
-              <button className="rounded-[5rem] w-56 h-[3.8rem] font-dgm text-[1.625rem] bg-[#2C2C2C] text-white cursor-pointer mb-5"> 다음 </button>
+              <button
+                type="button"
+                className="rounded-[5rem] w-56 h-[3.8rem] font-dgm text-[1.625rem] bg-[#2C2C2C] hover:bg-[#585858] text-white cursor-pointer mb-5"
+                onClick={handleButtonClick}
+              >
+                확인
+              </button>
               <a href="#">
                   <span className="text-[1.20rem] text-[#B2B2B2] font-dgm underline underline-offset-1">                    
                   로그인 하러가기
