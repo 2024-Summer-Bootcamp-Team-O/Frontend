e37513911e03f954289bb56e4670d09d921e18c8
dannyk20 <dannyk20@naver.com>
2024-07-18 22:46:15 +0900

    design: 시작페이지 로고 디자인 수정

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import '../index.css';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const StartPage: React.FC = () => { 
+  const navigate = useNavigate();
+  
   const handleButtonClick = () => {
     const audio = new Audio('src/assets/sounds/click.mp3');
     audio.play();
+    navigate('/signup');
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen bg-cover bg-main_s">
-        <h1 className="z-40 ml-24 font-normal text-center translate-y-48 text-9xl gradient-text-s font-dnf rotate-355 text-border">
-          걔, 킹받네 ?
-        </h1>
-        <div className="flex flex-row justify-end">
-          <img src="src/assets/images/background/bg_stand1.png" alt="male_a" className="z-10" />
-          <img src="src/assets/images/background/bg_stand2.png" alt="female_a" className="z-20 -ml-56" />
-          <img src="src/assets/images/background/bg_stand3.png" alt="female_b" className="z-30 -ml-20" />
-          <img src="src/assets/images/background/bg_stand4.png" alt="male_b" className="-ml-32 z-25" />
-        </div>
-        <Link to="/signup">
-        <button 
-          type="button" 
-          className="z-40 mb-10 text-6xl ml-[4.2rem] -translate-y-[13rem] font-normal text-center gradient-text font-dnf text-border-norm"
-          onClick={handleButtonClick}>
-          시작하기
-        </button>
-        </Link>
+    <div className="flex flex-col items-center justify-end w-screen h-screen bg-cover bg-main_s">
+      <div className='z-40 w-[60rem] h-[18rem] mt-4 ml-[5.5rem] translate-y-96'>
+        <img src="src/assets/images/others/logo_i.png" alt="logo" />
+      </div>
+      <button 
+        type="button" 
+        className="z-40 mb-10 text-6xl font-normal text-center gradient-text font-dnf text-border-norm translate-y-[60rem]"
+        onClick={handleButtonClick}
+      >
+        시작하기
+      </button>
+      <div className="flex flex-row items-end justify-center w-full">
+        <img src="src/assets/images/background/bg_stand1.png" alt="male_a" className="z-10" />
+        <img src="src/assets/images/background/bg_stand2.png" alt="female_a" className="z-20 -ml-56" />
+        <img src="src/assets/images/background/bg_stand3.png" alt="female_b" className="z-30 -ml-20" />
+        <img src="src/assets/images/background/bg_stand4.png" alt="male_b" className="-ml-32 z-25" />
+      </div>
     </div>
   );
 }
